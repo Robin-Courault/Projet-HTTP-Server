8f7ae7dad4e51fe34014cc7dd664537c51ec957b
Rayane LABZIZI <rayane.rlsm@gmail.com>
2026-03-27 11:18:02 +0100

    fix the writing of set cookies

--- ricm.web/impl/httpserver/itf/impl/HttpRicmletResponseImpl.java
+++ ricm.web/impl/httpserver/itf/impl/HttpRicmletResponseImpl.java
@@ -4,15 +4,27 @@ import httpserver.itf.HttpRequest;
 import httpserver.itf.HttpRicmletResponse;
 
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpRicmletResponseImpl extends HttpResponseImpl implements HttpRicmletResponse {
 
+    private Map<String, String> m_cookies = new HashMap<>();
     public HttpRicmletResponseImpl(HttpServer hs, HttpRequest req, PrintStream ps) {
         super(hs, req, ps);
     }
 
     @Override
     public void setCookie(String name, String value) {
-   	 	m_ps.println("Set-Cookie: " + name + "=" + value); // on envoie le cookie dans le header de la réponse avec le protocole HTTP
+    	m_cookies.put(name, value);
+    }
+    
+    @Override
+	public PrintStream beginBody() {
+    	for (Map.Entry<String, String> cookie : m_cookies.entrySet()) {
+			m_ps.println("Set-Cookie: " + cookie.getKey() + "=" + cookie.getValue());
+		}
+		m_ps.println(); 
+		return m_ps;
     }
 }
